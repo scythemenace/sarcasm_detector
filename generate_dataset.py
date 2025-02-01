5662751e7a09b20da41503942e3741daf4e1b0c8
Ankur Pandey <pankur216@gmail.com>
2025-02-01 17:02:18 -0500

    updated code

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,5 +1,6 @@
 import praw
 import csv
+import pandas as pd
 
 reddit = praw.Reddit(
     client_id="4vMXBeeBinRuBCS66IQSPg",
@@ -10,35 +11,167 @@ reddit = praw.Reddit(
 # print(reddit.read_only)
 
 data = []
-processed_data = []
 
-for submission in reddit.subreddit("nottheonion").top(limit=5000):
+for submission in reddit.subreddit("nottheonion").top(limit=1000):
     data.append({"text": submission.title, "label": ""})
 
-for submission in reddit.subreddit("bbc").top(limit=6000):
+for submission in reddit.subreddit("nottheonion").hot(limit=1000):
     data.append({"text": submission.title, "label": ""})
 
-for submission in reddit.subreddit("clevercomebacks").top(limit=5000):
+for submission in reddit.subreddit("nottheonion").new(limit=1000):
     data.append({"text": submission.title, "label": ""})
 
-for submission in reddit.subreddit("cnn").top(limit=6000):
+for submission in reddit.subreddit("nottheonion").controversial(limit=1000):
     data.append({"text": submission.title, "label": ""})
 
-for submission in reddit.subreddit("PrivateEye").top(limit=2000):
+# --------------------------------------------------------------
+
+
+for submission in reddit.subreddit("bbc").top(limit=1000):
     data.append({"text": submission.title, "label": ""})
 
-for submission in reddit.subreddit("Clickhole").top(limit=2000):
+for submission in reddit.subreddit("bbc").hot(limit=1000):
     data.append({"text": submission.title, "label": ""})
 
+for submission in reddit.subreddit("bbc").new(limit=1000):
+    data.append({"text": submission.title, "label": ""})
 
-for d in data:
-    if d not in processed_data:
-        if len(d["text"]) < 300 and len(d["text"].split(" ")) > 4:
-            processed_data.append(d)
+for submission in reddit.subreddit("bbc").controversial(limit=1000):
+    data.append({"text": submission.title, "label": ""})
+
+# --------------------------------------------------------------
+
+for submission in reddit.subreddit("clevercomebacks").top(limit=1000):
+    data.append({"text": submission.title, "label": ""})
+
+for submission in reddit.subreddit("clevercomebacks").hot(limit=1000):
+    data.append({"text": submission.title, "label": ""})
+
+for submission in reddit.subreddit("clevercomebacks").new(limit=1000):
+    data.append({"text": submission.title, "label": ""})
+
+for submission in reddit.subreddit("clevercomebacks").controversial(limit=1000):
+    data.append({"text": submission.title, "label": ""})
+
+# --------------------------------------------------------------
+
+for submission in reddit.subreddit("cnn").top(limit=1000):
+    data.append({"text": submission.title, "label": ""})
+
+for submission in reddit.subreddit("cnn").hot(limit=1000):
+    data.append({"text": submission.title, "label": ""})
+
+for submission in reddit.subreddit("cnn").new(limit=1000):
+    data.append({"text": submission.title, "label": ""})
+
+for submission in reddit.subreddit("cnn").controversial(limit=1000):
+    data.append({"text": submission.title, "label": ""})
+
+# --------------------------------------------------------------
+
+for submission in reddit.subreddit("PrivateEye").top(limit=1000):
+    data.append({"text": submission.title, "label": ""})
+
+for submission in reddit.subreddit("PrivateEye").hot(limit=1000):
+    data.append({"text": submission.title, "label": ""})
+
+for submission in reddit.subreddit("PrivateEye").new(limit=1000):
+    data.append({"text": submission.title, "label": ""})
+
+for submission in reddit.subreddit("PrivateEye").controversial(limit=1000):
+    data.append({"text": submission.title, "label": ""})
+
+# --------------------------------------------------------------
+
+
+for submission in reddit.subreddit("Clickhole").top(limit=1000):
+    data.append({"text": submission.title, "label": ""})
+
+for submission in reddit.subreddit("Clickhole").hot(limit=1000):
+    data.append({"text": submission.title, "label": ""})
+
+for submission in reddit.subreddit("Clickhole").new(limit=1000):
+    data.append({"text": submission.title, "label": ""})
+
+for submission in reddit.subreddit("Clickhole").controversial(limit=1000):
+    data.append({"text": submission.title, "label": ""})
+
+# --------------------------------------------------------------
+
+for submission in reddit.subreddit("worldnews").top(limit=1000):
+    data.append({"text": submission.title, "label": ""})
+
+for submission in reddit.subreddit("worldnews").hot(limit=1000):
+    data.append({"text": submission.title, "label": ""})
+
+for submission in reddit.subreddit("worldnews").new(limit=1000):
+    data.append({"text": submission.title, "label": ""})
+
+for submission in reddit.subreddit("worldnews").controversial(limit=1000):
+    data.append({"text": submission.title, "label": ""})
+
+# --------------------------------------------------------------
+
+for submission in reddit.subreddit("TheBabylonBee").top(limit=1000):
+    data.append({"text": submission.title, "label": ""})
+
+for submission in reddit.subreddit("TheBabylonBee").hot(limit=1000):
+    data.append({"text": submission.title, "label": ""})
+
+for submission in reddit.subreddit("TheBabylonBee").new(limit=1000):
+    data.append({"text": submission.title, "label": ""})
+
+for submission in reddit.subreddit("TheBabylonBee").controversial(limit=1000):
+    data.append({"text": submission.title, "label": ""})
+
+# --------------------------------------------------------------
+
+for submission in reddit.subreddit("politics").top(limit=1000):
+    data.append({"text": submission.title, "label": ""})
+
+for submission in reddit.subreddit("politics").hot(limit=1000):
+    data.append({"text": submission.title, "label": ""})
+
+for submission in reddit.subreddit("politics").new(limit=1000):
+    data.append({"text": submission.title, "label": ""})
+
+for submission in reddit.subreddit("politics").controversial(limit=1000):
+    data.append({"text": submission.title, "label": ""})
+
+# --------------------------------------------------------------
 
 
 with open("dataset.csv", "w", newline="") as csvfile:
     fieldnames = ["text", "label"]
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
-    writer.writerows(processed_data)
+    writer.writerows(data)
+
+# 1. Read CSV
+df = pd.read_csv("dataset.csv")
+
+# 2(a). For complete row duplicate
+df.drop_duplicates(inplace=True)
+
+# df = df[len(df.text) > ]
+
+# 3. Save then
+df.to_csv("data.csv", index=False)
+
+
+# --------------------------------------------------------------
+
+# for submission in reddit.subreddit("bbc").top(limit=1000):
+#     data.append({"text": submission.title, "label": ""})
+
+# for submission in reddit.subreddit("clevercomebacks").top(limit=1000):
+#     data.append({"text": submission.title, "label": ""})
+
+# for submission in reddit.subreddit("cnn").top(limit=1000):
+#     data.append({"text": submission.title, "label": ""})
+
+# for submission in reddit.subreddit("PrivateEye").top(limit=1000):
+#     data.append({"text": submission.title, "label": ""})
+
+# for submission in reddit.subreddit("Clickhole").top(limit=1000):
+#     data.append({"text": submission.title, "label": ""})
