ebf77422c4b119050eb1b5b5dc242fe4f2a4e5b8
Ankur Pandey <pankur216@gmail.com>
2025-02-01 16:04:49 -0500

    updated code

--- generate_dataset.py
+++ generate_dataset.py
@@ -10,46 +10,35 @@ reddit = praw.Reddit(
 # print(reddit.read_only)
 
 data = []
+processed_data = []
 
-for submission in reddit.subreddit("nottheonion").top(limit=2500):
+for submission in reddit.subreddit("nottheonion").top(limit=5000):
     data.append({"text": submission.title, "label": ""})
 
-for submission in reddit.subreddit("nottheonion").hot(limit=2500):
+for submission in reddit.subreddit("bbc").top(limit=6000):
     data.append({"text": submission.title, "label": ""})
 
-for submission in reddit.subreddit("bbc").top(limit=3000):
+for submission in reddit.subreddit("clevercomebacks").top(limit=5000):
     data.append({"text": submission.title, "label": ""})
 
-for submission in reddit.subreddit("bbc").hot(limit=3000):
+for submission in reddit.subreddit("cnn").top(limit=6000):
     data.append({"text": submission.title, "label": ""})
 
-for submission in reddit.subreddit("clevercomebacks").top(limit=2500):
+for submission in reddit.subreddit("PrivateEye").top(limit=2000):
     data.append({"text": submission.title, "label": ""})
 
-for submission in reddit.subreddit("clevercomebacks").hot(limit=2500):
+for submission in reddit.subreddit("Clickhole").top(limit=2000):
     data.append({"text": submission.title, "label": ""})
 
-for submission in reddit.subreddit("cnn").top(limit=3000):
-    data.append({"text": submission.title, "label": ""})
 
-for submission in reddit.subreddit("cnn").hot(limit=3000):
-    data.append({"text": submission.title, "label": ""})
-
-for submission in reddit.subreddit("PrivateEye").top(limit=500):
-    data.append({"text": submission.title, "label": ""})
-
-for submission in reddit.subreddit("PrivateEye").hot(limit=500):
-    data.append({"text": submission.title, "label": ""})
-
-for submission in reddit.subreddit("Clickhole").top(limit=500):
-    data.append({"text": submission.title, "label": ""})
-
-for submission in reddit.subreddit("Clickhole").hot(limit=500):
-    data.append({"text": submission.title, "label": ""})
+for d in data:
+    if d not in processed_data:
+        if len(d["text"]) < 300 and len(d["text"].split(" ")) > 4:
+            processed_data.append(d)
 
 
 with open("dataset.csv", "w", newline="") as csvfile:
     fieldnames = ["text", "label"]
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
-    writer.writerows(data)
+    writer.writerows(processed_data)
