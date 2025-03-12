7b19a0ae70a5dc6ce043b3194dab43ec561e1a99
Ankur Pandey <pankur216@gmail.com>
2025-03-12 01:33:14 -0400

    created training.py and changed core ground_labels.csv

---
+++ model_training/training.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Loading my dataset
+df = pd.read_csv("../ground_labels/ground_labels.csv")
+
+# Just checking the first few rows
+# print(df.head())
+#
+# # Number of rows Sarcastic vs Not-Sarcastic
+# print(df["label"].value_counts())
