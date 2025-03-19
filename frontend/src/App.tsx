4f61c07838514cbdf83ec2f90308f53149f47600
Ankur Pandey <pankur216@gmail.com>
2025-03-19 01:54:18 -0400

    fetched outputs from flask api using react

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,27 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import useDebounce from "./components/useDebounce";
+import axios from "axios";
+
+const apiUrl = import.meta.env.VITE_API_URL;
 
 function App() {
   const [inputText, setInputText] = useState("");
   const debouncedInputText = useDebounce(inputText);
+  const [outputText, setOutputText] = useState("");
 
-  useEffect(() => {}, [debouncedInputText]);
+  useEffect(() => {
+    axios
+      .post(`${apiUrl}/api/analyze`, {
+        text: debouncedInputText,
+      })
+      .then((response) => {
+        setOutputText(response.data.output);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [debouncedInputText]);
 
   return (
     <>
@@ -15,7 +30,16 @@ function App() {
         onChange={(e) => setInputText(e.target.value)}
         placeholder="Enter sarcastic text"
       ></input>
-      <p>{debouncedInputText}</p>
+      <p>
+        Input Text:{" "}
+        {debouncedInputText != "" ? debouncedInputText : "No string entered"}
+      </p>
+      <p>
+        Output:{" "}
+        {outputText == "1" && debouncedInputText != ""
+          ? "Sarcastic"
+          : "Not Sarcastic"}
+      </p>
     </>
   );
 }
